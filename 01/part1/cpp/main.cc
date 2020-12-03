2e639b5ea61192bd8460b1c39519c93cf9d7413d
Jan Hensel <ja_he@uni-bremen.de>
2020-12-03 18:20:28 +0100

    01: use std::unoredered_set instead of vector

--- 01/part1/cpp/main.cc
+++ 01/part1/cpp/main.cc
@@ -1,22 +1,30 @@
 #include <iostream>
 #include <vector>
+#include <unordered_set>
 #include <string>
 
 int
 main(void)
 {
-  std::vector<uint32_t> values;
+  std::unordered_set<uint32_t> values;
 
   uint32_t i;
   while (std::cin >> i) {
-    values.push_back(i);
+    values.insert(i);
   }
 
   for (const auto& val : values) {
-    if (std::find(values.begin(), values.end(), 2020-val) < values.end()) {
-      std::cout << val * (2020-val) << std::endl;
-      return EXIT_SUCCESS;
+    if (values.find(2020-val) != values.end()) {
+      if (val != 1010) {
+        std::cout << val * (2020-val) << std::endl;
+        return EXIT_SUCCESS;
+      }
     }
+  }
+
+  if (values.count(1010) > 1) {
+    std::cout << 1010*1010 << std::endl;
+    return EXIT_SUCCESS;
   }
 
   return EXIT_FAILURE;
