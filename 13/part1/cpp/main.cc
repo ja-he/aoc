e20a98193cf1db31c012222e27869d01c546df0f
Jan Hensel <ja_he@uni-bremen.de>
2020-12-13 13:08:05 +0100

    13: solve part 1
    This one was surprisingly easy and well-suited for the standard
    library algorithms.

---
+++ 13/part1/cpp/main.cc
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <set>
+#include <sstream>
+#include <string>
+#include <unordered_set>
+#include <vector>
+
+#include <cassert>
+
+using Timestamp = unsigned;
+using ID        = unsigned;
+
+int
+main(void)
+{
+  Timestamp earliest_departure;
+  std::cin >> earliest_departure;
+
+  std::vector<ID> buslines_in_service;
+  {
+    std::string id_str;
+    while (std::getline(std::cin, id_str, ','))
+    {
+      if (id_str[0] == 'x') continue;
+      buslines_in_service.push_back(std::stoi(id_str));
+    }
+  }
+
+  std::vector<std::pair<unsigned, ID>> wait_times_per_line;
+  std::transform(buslines_in_service.begin(),
+                 buslines_in_service.end(),
+                 std::back_inserter(wait_times_per_line),
+                 [earliest_departure](ID line) {
+                   Timestamp tmp       = earliest_departure % line;
+                   unsigned  wait_time = line - tmp;
+                   return std::pair<unsigned, ID>{ wait_time, line };
+                 });
+  auto [wait_time, line] =
+    *std::min_element(wait_times_per_line.begin(), wait_times_per_line.end());
+
+  std::cout << "answer: " << wait_time * line << std::endl;
+
+  return EXIT_SUCCESS;
+}
