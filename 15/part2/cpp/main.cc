925215615f6430e850a3ca153c1b67c03055c423
Jan Hensel <ja_he@uni-bremen.de>
2020-12-24 18:19:49 +0100

    15: solve part 2
    Solution as I indicated in the previous commit using a map.
    This took about 210 seconds to complete, so it should be improved,
    which I'll do in a later commit to this.

---
+++ 15/part2/cpp/main.cc
@@ -0,0 +1,57 @@
+#include <iomanip>
+#include <iostream>
+
+#include <algorithm>
+#include <numeric>
+
+#include <bitset>
+#include <map>
+#include <string>
+#include <vector>
+
+#define WANTED_INDEX 30000000
+
+using Number = unsigned;
+using Step   = unsigned;
+
+int
+main(void)
+{
+  int index = 1;
+  Number current;
+  std::map<Number, Step> numbers;
+
+  {
+    std::vector<Number> starting_nums;
+    std::string starting_str;
+    while (std::getline(std::cin, starting_str, ','))
+    {
+      starting_nums.push_back(std::stoul(starting_str));
+    }
+    std::for_each(starting_nums.begin(), starting_nums.end()-1,
+      [&index,&numbers](Number n){ numbers[n] = index; index++; });
+    current = starting_nums.back();
+  }
+
+  while (index < WANTED_INDEX)
+  {
+    if (numbers.contains(current)) {
+      Step step = numbers.at(current);
+      numbers[current] = index;
+      current = index - step;
+    } else {
+      numbers[current] = index;
+      current = 0;
+    }
+
+    std::cerr << "i = " << index << "\r" << std::flush;
+
+    index++;
+  }
+  std::cerr << std::endl;
+
+  std::cout << "number at " << WANTED_INDEX << ": " << current
+            << std::endl;
+
+  return EXIT_SUCCESS;
+}
