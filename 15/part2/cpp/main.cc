3a8a21dc24afccecf5b52b51f7599f502e25b9f7
Jan Hensel <ja_he@uni-bremen.de>
2020-12-24 23:32:42 +0100

    15: format code

--- 15/part2/cpp/main.cc
+++ 15/part2/cpp/main.cc
@@ -17,36 +17,34 @@ using Step   = uint32_t;
 int
 main(void)
 {
-  int index = 1;
-  Number current;
-  std::vector<Step> numbers(WANTED_INDEX);
-  std::fill(numbers.begin(), numbers.end(), 0);
+  Step              index = 1;
+  Number            current;
+  std::vector<Step> numbers(WANTED_INDEX, 0);
 
   {
     std::vector<Number> starting_nums;
-    std::string starting_str;
+    std::string         starting_str;
     while (std::getline(std::cin, starting_str, ','))
     {
       starting_nums.push_back(std::stoul(starting_str));
     }
-    std::for_each(starting_nums.begin(), starting_nums.end()-1,
-      [&index,&numbers](Number n){ numbers[n] = index; index++; });
+    std::for_each(starting_nums.begin(),
+                  starting_nums.end() - 1,
+                  [&index, &numbers](Number n) {
+                    numbers[n] = index;
+                    index++;
+                  });
     current = starting_nums.back();
   }
 
   while (index < WANTED_INDEX)
   {
     Step last_occurrence = numbers[current];
-
-    numbers[current] = index;
-
-    current = (last_occurrence) ? index - last_occurrence : 0;
-
+    numbers[current]     = index;
+    current              = (last_occurrence) ? index - last_occurrence : 0;
     index++;
   }
 
-  std::cout << "number at " << WANTED_INDEX << ": " << current
-            << std::endl;
-
+  std::cout << "number at " << WANTED_INDEX << ": " << current << std::endl;
   return EXIT_SUCCESS;
 }
